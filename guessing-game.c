e60fa65b5dd64216d3d0e6d47a68bb2249fbc75b
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-08-05 19:09:10 +0300

    Add files via upload

--- guessing-game.c
+++ guessing-game.c
@@ -5,14 +5,29 @@
 int main() {
     int user;
     int computer;
+    int count = 1;
     printf("Enter a guess: ");
     scanf("%d", &user);
 
     computer = rand() % 10 + 1;
 
     if (computer == user) {
-        printf("You Have won!");
+        printf("You Have won in 1 try!");
     } else {
-        printf("You have lost!");
+        while (computer != user) {
+            if (computer > user) {
+                printf("The number is bigger than what you entered! Try again\n Enter a guess: ");
+                scanf("%d", &user);
+                count = count + 1;
+
+            } else if (computer < user) {
+                printf("The number is less than what you entered! Try again\n Enter a guess: ");
+                scanf("%d", &user);
+                count = count + 1;
+
+            }
+        }
+        printf("You Have won! in %d trys",count);
+
     }
 }
